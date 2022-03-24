f15895facf570fa9c9d03c3d87a1dd649cb586d3
Amandda Luz <amandaomariano@hotmail.com>
2022-03-23 23:01:47 -0300

    first commit

---
+++ app/src/main/java/com/aluz/testeloop/MainActivity.java
@@ -0,0 +1,30 @@
+package com.aluz.testeloop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button BotaoJogar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BotaoJogar = findViewById(R.id.play);
+
+        BotaoJogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent inicio = new Intent(getApplicationContext(), MainActivity2.class);
+                startActivity(inicio);
+
+            }
+        });
+    }
+}
